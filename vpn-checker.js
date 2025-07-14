d21ae4c946de08b3f00b494579d8e697cb17270e
waversound <syutkinxvslv@yandex.ru>
2025-07-15 01:06:22 +0300

    Update vpn-checker.js

--- vpn-checker.js
+++ vpn-checker.js
@@ -1,30 +1,13 @@
 (function () {
-    // Функция конвертации кода страны в эмодзи-флаг
-    function getFlagEmoji(countryCode) {
-        if (!countryCode || countryCode.length !== 2) return '';
-        const codePoints = countryCode
-            .toUpperCase()
-            .split('')
-            .map(c => 127397 + c.charCodeAt());
-        return String.fromCodePoint(...codePoints);
-    }
-
-    // Показ уведомления с предупреждением
     function showWarning(country) {
-        const flag = getFlagEmoji(country);
-        const message = `${flag} Обнаружен VPN или Вы вне России (${country}). Пожалуйста, отключите VPN для корректной работы.`;
-
-        if (!localStorage.getItem('vpnWarningShown')) {
-            if (window.Lampa && Lampa.Noty && typeof Lampa.Noty.show === 'function') {
-                Lampa.Noty.show(message);
-                localStorage.setItem('vpnWarningShown', 'true');
-            } else {
-                console.log('[VPN Plugin] Lampa.Noty.show не доступен');
-            }
+        const message = `Внимание! Вы используете VPN или находитесь вне России (${country}). Отключите VPN для корректной работы.`;
+        if (window.Lampa && Lampa.Noty && typeof Lampa.Noty.show === 'function') {
+            Lampa.Noty.show(message);
+        } else {
+            console.log('[VPN Plugin] Lampa.Noty.show не доступен');
         }
     }
 
-    // Проверка IP и страны
     function checkVPN() {
         fetch('https://freegeoip.app/json/')
             .then(res => res.json())
@@ -34,7 +17,6 @@
                     showWarning(country);
                 } else {
                     console.log('[VPN Plugin] IP из РФ, всё в порядке');
-                    localStorage.removeItem('vpnWarningShown');
                 }
             })
             .catch(err => {
@@ -42,7 +24,6 @@
             });
     }
 
-    // Стартуем при загрузке Lampa
     if (window.Lampa) {
         checkVPN();
     } else {
