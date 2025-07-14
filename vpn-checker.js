2dceba87d92edbd23c1feb4bba79819a4a0f4eba
waversound <syutkinxvslv@yandex.ru>
2025-07-15 01:29:48 +0300

    Update vpn-checker.js

--- vpn-checker.js
+++ vpn-checker.js
@@ -1,4 +1,22 @@
 (function () {
+    // Сопоставление английских названий стран с русскими
+    const countryTranslations = {
+        'Russia': 'Россия',
+        'Finland': 'Финляндия',
+        'United States': 'США',
+        'Germany': 'Германия',
+        'Netherlands': 'Нидерланды',
+        'France': 'Франция',
+        'Sweden': 'Швеция',
+        'United Kingdom': 'Великобритания',
+        'Canada': 'Канада',
+        'Ukraine': 'Украина',
+        'Singapore': 'Сингапур',
+        'Norway': 'Норвегия',
+        'Kazakhstan': 'Казахстан',
+        // Добавляй по необходимости
+    };
+
     function getCountryFlag(code) {
         return code.toUpperCase().replace(/./g, char =>
             String.fromCodePoint(127397 + char.charCodeAt())
@@ -7,7 +25,7 @@
 
     function showCustomBanner(message) {
         const existing = document.getElementById('vpn-warning');
-        if (existing) existing.remove(); // Удалим предыдущее, если есть
+        if (existing) existing.remove();
 
         const div = document.createElement('div');
         div.id = 'vpn-warning';
@@ -23,11 +41,12 @@
         div.style.zIndex = '9999';
         div.style.textAlign = 'center';
         div.style.boxShadow = '0 0 15px rgba(0,0,0,0.5)';
+        div.style.maxWidth = '90%';
+
         div.innerText = message;
 
         document.body.appendChild(div);
 
-        // Удаляем уведомление через 5 секунд
         setTimeout(() => {
             div.remove();
         }, 5000);
@@ -41,13 +60,14 @@
             })
             .then(data => {
                 const countryCode = data.country_code || '';
-                const countryName = data.country || '';
+                const countryNameEng = data.country || '';
+                const countryNameRu = countryTranslations[countryNameEng] || countryNameEng;
                 const flag = getCountryFlag(countryCode);
 
-                console.log(`[VPN Plugin] Обнаружена страна: ${countryName} (${countryCode})`);
+                console.log(`[VPN Plugin] Обнаружена страна: ${countryNameEng} (${countryCode})`);
 
                 if (countryCode !== 'RU') {
-                    const message = `${flag} Вы находитесь в стране: ${countryName}. Возможно, включён VPN. Отключите его для стабильной работы.`;
+                    const message = `Вы находитесь в стране: ${countryNameRu} ${flag}. Возможно, включён VPN. Отключите его для стабильной работы.`;
                     showCustomBanner(message);
                 } else {
                     console.log('[VPN Plugin] IP из РФ, всё в порядке.');
